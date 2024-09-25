82f473ac3f8aa21bb97402e371d59e5ba9a7b412
josteveadekanbi <adekanbijosteve@gmail.com>
2024-09-25 23:03:19 +0100

    Project initialisation

---
+++ android/app/src/main/kotlin/com/josteve/magic/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.josteve.magic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/framework/theme/colors/app_colors.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  final Color alwaysWhite;
+  final Color alwaysBlack;
+  final Color transparent;
+  final Color always00E6B4;
+  final Color alwaysfbdc13;
+  final Color always1c1c1e;
+  final Color always26292C;
+  final Color always878F97;
+  final Color alwaysD9DBDE;
+  final Color always3D4347;
+  final Color alwaysBDC2C6;
+  final Color alwaysA2A9AF;
+  final Color alwaysRed;
+  final Color always6C757D;
+
+  AppColors({
+    this.alwaysWhite = Colors.white,
+    this.alwaysBlack = Colors.black,
+    this.transparent = Colors.transparent,
+    this.always00E6B4 = const Color(0xFF00E6B4),
+    this.alwaysfbdc13 = const Color(0xFFfbdc13),
+    this.always1c1c1e = const Color(0xFF1c1c1e),
+    this.always26292C = const Color(0xFF26292C),
+    this.always878F97 = const Color(0xFF878F97),
+    this.alwaysD9DBDE = const Color(0xFFD9DBDE),
+    this.always3D4347 = const Color(0xFF3D4347),
+    this.alwaysBDC2C6 = const Color(0xFFBDC2C6),
+    this.alwaysA2A9AF = const Color(0xFFA2A9AF),
+    this.alwaysRed = const Color(0xFFEF422A),
+    this.always6C757D = const Color(0xFF6C757D),
+  });
+}
